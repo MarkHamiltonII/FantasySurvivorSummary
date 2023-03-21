91e5155e91d49aff4e369979cc49336d9eb418d5
MarkHamiltonII <markhamiltonii@gmail.com>
2023-03-21 00:07:48 -0500

    Added grayed out players and who still counts towards points

--- summary-page/src/components/Ranking.js
+++ summary-page/src/components/Ranking.js
@@ -1,6 +1,22 @@
 export default function Ranking({ username }) {
 
     const ranking = require("./../data/league2/"+username+".json")
+    const current_tribal = require("./../data/league2/current_tribal.json")
+    const remaining = current_tribal.length
+
+
+    const getColor = (castaway) => {
+        let c = current_tribal.filter(ct => ct.id === castaway.id);
+        return c.length > 0 ? `bg-${castaway.tribeColor}` : 'bg-Slate'
+    }
+
+    const getCutoff = (index) => {
+        console.log(remaining)
+        console.log(index)
+        if (index === remaining) {return 'cutoff out'}
+        if (index >= remaining) {return 'out'}
+        else {return ''}
+    }
 
     return (
         <div className="ranking-container">
@@ -15,7 +31,7 @@ export default function Ranking({ username }) {
                 </thead>
                 <tbody>
                     {ranking.castaways.map((castaway, index) => (
-                        <tr className={`bg-${castaway.tribeColor}`} key={index}>
+                        <tr className={`${getColor(castaway)} ${getCutoff(index)}`} key={index}>
                             <td>{index + 1}</td>
                             <td className="headshot"><img src={castaway.iconURL} alt='Castaway Portrait' /></td><td><a href={castaway.pageURL} title={`${castaway.firstName}'s Survivor Wiki page`} target="_blank" rel="noreferrer" to={`/castaway${castaway.id}`}><span>{castaway.firstName + " " + castaway.lastName}</span></a></td>
                         </tr>
